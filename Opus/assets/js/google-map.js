fd8a94cd4cea217745a7a18ef62d25d9bc38ad64
Juan <juanandresleandrorodriguez@gmail.com>
2022-04-18 21:05:02 -0500

    feat: Progesando en el landing page

--- Opus/assets/js/google-map.js
+++ Opus/assets/js/google-map.js
@@ -1,73 +1,71 @@
-google.maps.event.addDomListener(window, 'load', init);
+google.maps.event.addDomListener(window, "load", init);
 
 var map;
 
 function init() {
-	var mapOptions = {
-		center: new google.maps.LatLng(29.7063428,-95.40526,17),
-		zoom: 17,
-		zoomControl: true,
-		zoomControlOptions: {
-			style: google.maps.ZoomControlStyle.SMALL,
-		},
-		disableDoubleClickZoom: true,
-		mapTypeControl: true,
-		mapTypeControlOptions: {
-			style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-		},
-		scaleControl: false,
-		scrollwheel: true,
-		streetViewControl: true,
-		draggable : true,
-		overviewMapControl: false,
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		styles: [
-		{
-			featureType: "landscape",
-			stylers: [
-			{ saturation: -100 },
-			{ lightness: 65 },
-			{ visibility: "on" }
-			]
-		},{
-			featureType: "poi",
-			stylers: [
-			{ saturation: -100 },
-			{ lightness: 51 },
-			{ visibility: "simplified" }
-			]
-		},{
-			featureType: "road.highway",
-			stylers: [
-			{ saturation: -100 },
-			{ visibility: "simplified" }
-			]
-		},{
-			featureType: "road.arterial",
-			stylers: [
-			{ saturation: -100 },
-			{ lightness: 30 },
-			{ visibility: "on" }
-			]
-		},{
-			featureType: "road.local",
-			stylers: [
-			{ saturation: -100 },
-			{ lightness: 40 },
-			{ visibility: "on" }
-			]
-		},{
-			featureType: "transit",
-			stylers: [
-			{ saturation: -100 },
-			{ visibility: "simplified" }
-			]
-		},{
-			featureType: "administrative.province",
-			stylers: [
-			{ visibility: "off" }
-			]
-	/** /
+  var mapOptions = {
+    center: new google.maps.LatLng(4.626689, -74.165184),
+    zoom: 17,
+    zoomControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.SMALL,
+    },
+    disableDoubleClickZoom: true,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+    },
+    scaleControl: false,
+    scrollwheel: true,
+    streetViewControl: true,
+    draggable: true,
+    overviewMapControl: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [
+      {
+        featureType: "landscape",
+        stylers: [
+          { saturation: -100 },
+          { lightness: 65 },
+          { visibility: "on" },
+        ],
+      },
+      {
+        featureType: "poi",
+        stylers: [
+          { saturation: -100 },
+          { lightness: 51 },
+          { visibility: "simplified" },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        stylers: [{ saturation: -100 }, { visibility: "simplified" }],
+      },
+      {
+        featureType: "road.arterial",
+        stylers: [
+          { saturation: -100 },
+          { lightness: 30 },
+          { visibility: "on" },
+        ],
+      },
+      {
+        featureType: "road.local",
+        stylers: [
+          { saturation: -100 },
+          { lightness: 40 },
+          { visibility: "on" },
+        ],
+      },
+      {
+        featureType: "transit",
+        stylers: [{ saturation: -100 }, { visibility: "simplified" }],
+      },
+      {
+        featureType: "administrative.province",
+        stylers: [{ visibility: "off" }],
+        /** /
 		},{
 			featureType: "administrative.locality",
 			stylers: [
@@ -79,38 +77,33 @@ function init() {
 				{ visibility: "on" }
 			]
 			/**/
-		},{
-			featureType: "water",
-			elementType: "labels",
-			stylers: [
-			{ visibility: "on" },
-			{ lightness: -25 },
-			{ saturation: -100 }
-			]
-		},{
-			featureType: "water",
-			elementType: "geometry",
-			stylers: [
-			{ hue: "#ffff00" },
-			{ lightness: -25 },
-			{ saturation: -97 }
-			]
-		}
-		],
-		
-	}
+      },
+      {
+        featureType: "water",
+        elementType: "labels",
+        stylers: [
+          { visibility: "on" },
+          { lightness: -25 },
+          { saturation: -100 },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ hue: "#ffff00" }, { lightness: -25 }, { saturation: -97 }],
+      },
+    ],
+  };
 
-	var mapElement = document.getElementById('map');
-	var map = new google.maps.Map(mapElement, mapOptions);
-	var locations = [
-	
-	];
+  var mapElement = document.getElementById("map");
+  var map = new google.maps.Map(mapElement, mapOptions);
+  var locations = [];
 
-	for (i = 0; i < locations.length; i++) {
-		marker = new google.maps.Marker({
-			icon: '',
-			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-			map: map
-		});
-	}
+  for (i = 0; i < locations.length; i++) {
+    marker = new google.maps.Marker({
+      icon: "",
+      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      map: map,
+    });
+  }
 }
